122a69d6e739183f7570f06939dd0890e06929f2
danricardo88 <daniel.ricardo88@outlook.com>
2022-08-18 23:46:09 -0300

    req 3 ok - eu acho vms ver lá no lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('se é exibida na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favoritesText = screen.getByText(/No favorite pokemon found/i);
+    expect(favoritesText).toBeInTheDocument();
+  });
+
+  // test('se são exibidos todos os cards de pokémons favoritados', () => {
+  //   const { history } = renderWithRouter(<FavoritePokemons />);
+  // });
+});
